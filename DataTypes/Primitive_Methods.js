eb584dfbcd31128622a5385240a767998d769f8a
Mohammad Ashfan <mohammadashfan12>
2020-10-13 10:55:13 +0530

    updated programs

--- DataTypes/Primitive_Methods.js
+++ DataTypes/Primitive_Methods.js
@@ -11,11 +11,6 @@ function cases(str) {
 }
 console.log(cases('JAVASCRIPT'))
 
-//2. Setting the fractional number to n decimal points
-let num = 20.223536
-console.log(num.toFixed(2))
-
-
 //Display type of DataType
 function typeOfType() {
     console.log(typeof 2);
@@ -23,7 +18,7 @@ function typeOfType() {
     console.log(typeof new Number(0));
     let zero = new Number(0);
     if (zero) {
-        console.log("zero is truthy!?!");
+        console.log("zero is true");
     }
 }
 
--- Number.js
+++
@@ -1,3 +0,0 @@
-let billion = 1000000000;
-
-console.log(7.3e9 )
